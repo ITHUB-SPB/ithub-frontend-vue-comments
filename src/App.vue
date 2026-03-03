edfca80c0608d31e53f2aa4b601b233a70af29df
Lex Trofimov <daslef93@gmail.com>
2026-03-03 13:04:12 +0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { useRecordsStore } from './stores/records';
+
+const recordsStore = useRecordsStore()
+console.log(recordsStore.posts)
 </script>
 
 <template>
   <header></header>
-  <main></main>
+  <main>
+    <p v-for="record in recordsStore.posts" :key="record.id">
+      {{ record.title }}
+      <hr>
+      <p v-for="comment in record.comments" :key="record.id + '_' + comment.id">
+        {{ comment.text }}
+      </p>
+    </p>
+  </main>
 </template>
 
 <style scoped>
